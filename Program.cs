bd7cb7ebc2346924582c05330c2fc94c030e2f6c
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2024-05-08 20:51:57 -0300

    feat: CellPhone system challenge resolved

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone  = new Iphone("123456876", "Modelo 1", "1234", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("TodoList");
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("865213561", "Modelo 2", "65346", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Pomodoro");
